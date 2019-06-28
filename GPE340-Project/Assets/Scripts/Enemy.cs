7dc920e2bcca7684cb13030d85b3c5308e8b5595
swoofz <jordanhall.1995@yahoo.com>
2019-06-27 18:54:52 -0700

    Add Nav Mesh

---
+++ GPE340-Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform target;
+
+    private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private Vector3 desiredVelocity;
+
+    private void Awake() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        navMeshAgent.SetDestination(target.position);
+        Vector3 input = navMeshAgent.desiredVelocity;
+        desiredVelocity = Vector3.MoveTowards(desiredVelocity, navMeshAgent.desiredVelocity, navMeshAgent.acceleration * Time.deltaTime);
+        input = transform.InverseTransformDirection(desiredVelocity);
+        animator.SetFloat("Horizontal", input.x);
+        animator.SetFloat("Vertical", input.z);
+    }
+
+    private void OnAnimatorMove() {
+        navMeshAgent.velocity = animator.velocity;
+    }
+}
